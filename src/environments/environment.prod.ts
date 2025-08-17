9c57356a66b1c1062a84d88d3f4b31515c6fe500
nilesh <nileshvanjare993@gmail.com>
2025-08-17 21:31:23 +0530

    newpostcrud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl : 'https://post-01-167d8-default-rtdb.asia-southeast1.firebasedatabase.app'
+   bastUrl: 'https://post-01-167d8-default-rtdb.asia-southeast1.firebasedatabase.app'
 };
